748b641f5e44b821b81405e81e68a0bd210c5095
faris <faris38kanbur@gmail.com>
2021-04-15 19:06:21 +0200

    add array and regex

--- function.test.js
+++ function.test.js
@@ -22,3 +22,32 @@ test('Should be null', () => {
 test('should be falsy', () => {
     expect(functions.checkValue()).toBeFalsy();
 })
+
+
+test('User should be Brad Traversy object ', () => {
+    expect(functions.createUser()).toStrictEqual({
+        firstName: 'faris',
+        lastName: 'Kanbur'
+    });
+});
+
+
+//LessThan and GreaterThan
+test('Should bu under 1600 ', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1+load2).toBeLessThan(1600);
+    // expect(load1+load2).toBeGreaterThan(1800);
+})
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').toMatch(/teaM/i);
+})
+
+//Array
+test('Admin should be in username ', () => {
+   usernames = ['faris', 'kanbur', 'cem']
+    expect(usernames).toContain('fariS');
+})
+
